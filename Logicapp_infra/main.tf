ac2f9981e94b271ed909b1d9c128445733124f77
Selmouni Abdelilah <abdelilahselmouni1937@gmail.com>
2024-01-26 17:50:46 +0100

    Update

--- Logicapp_infra/main.tf
+++ Logicapp_infra/main.tf
@@ -1,8 +1,9 @@
+#create the resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
-
+#create the storage account for Logic App
 resource "azurerm_storage_account" "storageacc" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
@@ -10,7 +11,7 @@ resource "azurerm_storage_account" "storageacc" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
-
+#create the app service plan
 resource "azurerm_app_service_plan" "svcplan" {
   name                = "logicapp-test-service-plan"
   location            = azurerm_resource_group.rg.location
@@ -21,7 +22,7 @@ resource "azurerm_app_service_plan" "svcplan" {
     size = "WS1"
   }
 }
-
+#create the standard Logic App
 resource "azurerm_logic_app_standard" "logicapp" {
   name                       = var.logic_app_name
   location                   = azurerm_resource_group.rg.location
@@ -42,6 +43,7 @@ resource "azurerm_logic_app_standard" "logicapp" {
     type = "SystemAssigned"
   }
 }
+#create the Azure Resource Manager api connection 
 resource "azurerm_resource_group_template_deployment" "arm_api_connection" {
   name                = "arm-api-connection"
   resource_group_name = azurerm_resource_group.rg.name
@@ -53,6 +55,7 @@ resource "azurerm_resource_group_template_deployment" "arm_api_connection" {
   })
   deployment_mode = "Incremental"
 }
+#create the Slack api connection 
 resource "azurerm_resource_group_template_deployment" "slack_api_connection" {
   name                = "slack-api-connection"
   resource_group_name = azurerm_resource_group.rg.name
@@ -64,6 +67,7 @@ resource "azurerm_resource_group_template_deployment" "slack_api_connection" {
   })
   deployment_mode = "Incremental"
 }
+#create the Slack api connection access policy
 resource "azurerm_resource_group_template_deployment" "slack_api_connection_access_policy" {
   name                = "slack-api-connection-access-policy"
   resource_group_name = azurerm_resource_group.rg.name
@@ -80,6 +84,7 @@ resource "azurerm_resource_group_template_deployment" "slack_api_connection_acce
   })
   deployment_mode = "Incremental"
 }
+#create the Azure Resource Manager api connection access policy
 
 resource "azurerm_resource_group_template_deployment" "arm_api_connection_access_policy" {
   name                = "arm-api-connection-access-policy"
